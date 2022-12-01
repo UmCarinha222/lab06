2aeefbac09cc2e90d049b3f8d6b8e566464ef63e
CauãDias <94485488+UmCarinha222@users.noreply.github.com>
2022-12-01 20:42:26 -0300

    Create ex04.c

---
+++ ex04.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int A[5];
+  int maior = 0;
+  int menor = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    printf("Digite o %dº valor: ", i + 1);
+    scanf("%d", &A[i]);
+    if (A[i] > A[maior])
+    {
+      maior = i;
+    }
+    if (A[i] < A[menor])
+    {
+      menor = i;
+    }
+  }
+  printf("Maior: %d\n", maior);
+  printf("Menor: %d\n", menor);
+  return 0;
+}
