308f654d8d71af00171cd46c2c4c98f496e80892
CauãDias <94485488+UmCarinha222@users.noreply.github.com>
2022-12-01 20:45:36 -0300

    Create ex07.c

---
+++ ex07.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int A[6];
+  for (int i = 0; i < 6; i++)
+  {
+    printf("Digite o %dº valor: ", i + 1);
+    scanf("%d", &A[i]);
+  }
+  int soma_pares = 0;
+  int qtd_impares = 0;
+  for (int i = 0; i < 6; i++)
+  {
+    if (A[i] % 2 == 0)
+    {
+      printf("Par: %d\n", A[i]);
+      soma_pares += A[i];
+    }
+    else
+    {
+      printf("Ímpar: %d\n", A[i]);
+      qtd_impares++;
+    }
+  }
+  printf("Soma dos pares: %d\n", soma_pares);
+  printf("Quantidade de ímpares: %d\n", qtd_impares);
+  return 0;
+}
