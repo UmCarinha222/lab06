a24fa563173b3f9267b95cda17c146cc9b8f5a02
CauãDias <94485488+UmCarinha222@users.noreply.github.com>
2022-12-01 20:47:24 -0300

    Create ex_01.c

---
+++ ex_01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int A[4][4];
+  int count = 0;
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      printf("Digite o valor da linha %d e coluna %d: ", i + 1, j + 1);
+      scanf("%d", &A[i][j]);
+      if (A[i][j] > 10)
+      {
+        count++;
+      }
+    }
+  }
+  printf("Valores maiores que 10: %d\n", count);
+  return 0;
+}
